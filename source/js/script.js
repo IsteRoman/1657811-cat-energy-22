0bf5f17c9c0365501b9b1a1f445a84aee108be0f
RomanIst <demonatr@mail.ru>
2021-04-26 22:18:26 +0500

    апи2

--- source/js/script.js
+++ source/js/script.js
@@ -153,8 +153,42 @@ const validation = function() {
   });
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+
+  if (document.querySelector('#map')) {
+    ymaps.ready(function() {
+      var myMap = new ymaps.Map('map', {
+        center: [59.938635, 30.323118],
+        zoom: 16
+      }, {
+        searchControlProvider: 'yandex#search'
+      }),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/map-element/map-pin-mobile.png',
+        iconImageSize: [30, 42],
+
+            iconImageOffset: [-5, -38]
+
+      });
+
+      myMap.controls.remove('geolocationControl');
+      myMap.controls.remove('searchControl');
+      myMap.controls.remove('trafficControl');
+      myMap.controls.remove('typeSelector');
+      myMap.controls.remove('fullscreenControl');
+      myMap.controls.remove('rulerControl');
+      myMap.behavior.disable(['scrollZoom']);
+      myMap.geoObjects.add(myPlacemark);
+    });
+  }
+})
+
 mobileMenuNav();
 
 sliderInit();
 
 validation();
+
+apiMap();
